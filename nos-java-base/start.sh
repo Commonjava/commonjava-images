47785e5f267b7fec129c1739989c73b3a361e229
John Casey <jdcasey@commonjava.org>
2019-11-14 14:17:46 -0600

    refactor to a common base image for nos java services

---
+++ nos-java-base/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source /etc/profile
+
+set -x
+
+if [ -d /deployment/scripts ]; then
+    for script in $(ls -1 /deployment/scripts/*.sh); do
+        exec $script
+    done
+fi
+
+JAVA=$(which java)
+$JAVA -version 2>&1 > /dev/null
+if [ $? != 0 ]; then
+  PATH=${JAVA_HOME}/bin:${PATH}
+  JAVA=${JAVA_HOME}/bin/java
+fi
+
+exec $JAVA $JAVA_OPTS -jar /deployment/app.jar "$@"
