c9256153edf94a65f9173803f2c6fa4369b19c22
Jeremy Whiting <jwhiting@redhat.com>
2020-06-03 12:02:51 +0100

    Remove dynamic user support completely.

--- indy/setup-user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# configure dynamic user for containers in Openshift 
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
-envsubst < /opt/passwd.template > /tmp/passwd
-export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
-export NSS_WRAPPER_PASSWD=/tmp/passwd
-export NSS_WRAPPER_GROUP=/etc/group
