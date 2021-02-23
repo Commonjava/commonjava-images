84ccdd01909dc7f3c3b1445cd4b0465d8c023a9d
Wenjie Guo <wguo@redhat.com>
2021-02-23 15:46:23 +0800

    Fix the netty-transport-native-epoll warning
    Another way to fix the warning is to add the following dependency:
    ```
    <dependency>
      <groupId>io.netty</groupId>
      <artifactId>netty-transport-native-epoll</artifactId>
      <version>${netty.version}</version>
      <classifier>linux-x86_64</classifier>
    </dependency>
    ```

--- pathmap-storage-service/start-service.sh
+++ pathmap-storage-service/start-service.sh
@@ -6,4 +6,4 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
 export JAVA_CMD=$JAVA_HOME/bin/java
 
 cd /opt/pathmap-storage-service/
-$JAVA_CMD $JAVA_OPTS -jar ./pathmap-storage-service-runner.jar
+$JAVA_CMD $JAVA_OPTS -Dcom.datastax.driver.FORCE_NIO=true  -jar ./pathmap-storage-service-runner.jar
