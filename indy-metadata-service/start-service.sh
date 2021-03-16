93c425846dbee7d94b14151206e619af2ca5b478
Wenjie Guo <wguo@redhat.com>
2021-03-16 14:53:14 +0800

    Update the start-service command

--- indy-metadata-service/start-service.sh
+++ indy-metadata-service/start-service.sh
@@ -6,4 +6,4 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
 export JAVA_CMD=$JAVA_HOME/bin/java
 
 cd /opt/indy-metadata-service/
-$JAVA_CMD $JAVA_OPTS -Dcom.datastax.driver.FORCE_NIO=true  -jar ./indy-metadata-service-runner.jar
+$JAVA_CMD $JAVA_OPTS -jar ./indy-metadata-service-runner.jar
