d5279e058ca47cc8a2e877bad1010fb2930b560c
ruhan <ruhan@redhat.com>
2022-07-18 10:57:35 +0800

    Remove useless pathmap-storage-service/start.sh

--- pathmap-storage-service/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Hello! pathmap storage service starts!"
-
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
-export JAVA_CMD=$JAVA_HOME/bin/java
-
-cd /deployment
-$JAVA_CMD $JAVA_OPTS -jar ./pathmap-storage-service-runner.jar
